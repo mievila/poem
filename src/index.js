052e8f17481c5df4bbea2e71da463c890681fa89
mievila <akeyialefaith0@gmail.com>
2025-04-18 17:01:53 +0100

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("welcome")
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+        strings: "No matter how the skies my be",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
